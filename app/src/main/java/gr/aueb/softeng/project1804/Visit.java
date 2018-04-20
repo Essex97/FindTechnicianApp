e330e248cb960da0040bd9de0b0e5fddef2bb81e
dimitrisstaratzis <jstaratzi@icloud.com>
2018-04-20 17:59:14 +0300

    Appointment, Evaluation, Scale, Payment and Visit classes have been submitted.

--- app/src/main/java/gr/aueb/softeng/project1804/Visit.java
+++ app/src/main/java/gr/aueb/softeng/project1804/Visit.java
@@ -24,44 +24,98 @@ public class Visit
         this.technician = technician;
     }
 
+    /**
+     * @return the date that the visit will take place
+     */
     public Date getDate()
     {
         return date;
     }
 
+    /**
+     * @param date sets the Visit's date to date
+     */
     public void setDate(Date date)
     {
         this.date = date;
     }
 
+    /**
+     * @return the request that initialized the visit
+     */
     public Request getRequest()
     {
         return request;
     }
 
+    /**
+     * @return the resources that will be used during the visit
+     */
     public ArrayList<Resource> getResources()
     {
         return resources;
     }
 
+    /**
+     * @param resources sets the Visit's resources to resources
+     */
     public void setResources(ArrayList<Resource> resources)
     {
         this.resources = resources;
     }
 
+    /**
+     * @return the payment of the visit
+     */
     public Payment getPayment()
     {
         return payment;
     }
 
+    /**
+     * @return the technician that will attend
+     */
     public Technician getTechnician()
     {
         return technician;
     }
 
+    /**
+     * This method will create a Payment object as soon as the customer pays for the service
+     * @param amount the amount of money
+     */
     public void createPayment(double amount)
     {
         payment = new Payment(amount);
     }
 
+    /**
+     * @param o object to compare with the current
+     * @return true or false
+     */
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Visit visit = (Visit) o;
+
+        if (!date.equals(visit.date)) return false;
+        if (!request.equals(visit.request)) return false;
+        if (!resources.equals(visit.resources)) return false;
+        if (payment != null ? !payment.equals(visit.payment) : visit.payment != null) return false;
+        return technician.equals(visit.technician);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = date.hashCode();
+        result = 31 * result + request.hashCode();
+        result = 31 * result + resources.hashCode();
+        result = 31 * result + (payment != null ? payment.hashCode() : 0);
+        result = 31 * result + technician.hashCode();
+        return result;
+    }
 }
