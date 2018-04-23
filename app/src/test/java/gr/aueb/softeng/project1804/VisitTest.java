033a0e623af8a4871ce97008ecac831eee84b14b
Essex97 <stratosxen@gmail.com>
2018-04-24 01:48:33 +0300

    Add some Tests

--- app/src/test/java/gr/aueb/softeng/project1804/VisitTest.java
+++ app/src/test/java/gr/aueb/softeng/project1804/VisitTest.java
@@ -40,7 +40,7 @@ public class VisitTest {
         ofserv = new ArrayList<OfferedService>();
         ofserv.add(new OfferedService(tech, s1, 23.4));
         ofserv.add(new OfferedService(tech, s2, 255));
-        rq = new Request(new Date(2017, 3, 13), new Time(21, 24, 45), cst, (ArrayList<OfferedService>) ofserv);
+        rq = new Request(new Date(2017, 3, 13), new Time(21, 24, 45),tech, cst, ofserv);
         visit = new Visit(rq);
     }
 
@@ -73,5 +73,51 @@ public class VisitTest {
         Visit visit2 = new Visit(rq1);
         Assert.assertFalse(visit.equals(visit2));
     }
+
+    @Test
+    public void getDateTest(){
+        Assert.assertTrue(visit.getDate().equals(new Date(2017, 3, 13)) );
+    }
+
+    @Test
+    public void setDateTest(){
+        visit.setDate(new Date(2019, 4, 34));
+        Assert.assertTrue(visit.getDate().equals(new Date(2019, 4, 34)) );
+    }
+
+    @Test
+    public void getRequestTest(){
+        Assert.assertTrue(visit.getRequest().equals(rq) );
+    }
+
+    @Test
+    public void getPaymentTest(){
+        Assert.assertNull(visit.getPayment());
+    }
+
+    @Test
+    public void getTechnicianTest(){
+        Assert.assertTrue(visit.getTechnician().equals(tech) );
+    }
+
+    @Test
+    public void getCustomerTest(){
+        Assert.assertTrue(visit.getCustomer().equals(cst) );
+    }
+
+    @Test
+    public void getRecourcesTest(){
+        Assert.assertNull(visit.getResources());
+    }
+
+    @Test
+    public void setRecourcesTest(){
+       ArrayList<Resource> resources = new ArrayList<Resource>();
+       resources.add(new Resource("solhnas", 2, 2));
+       visit.setResources(resources);
+       Assert.assertTrue(visit.getResources().equals(resources));
+    }
+
+    
 
 }
